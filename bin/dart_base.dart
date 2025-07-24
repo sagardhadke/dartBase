06344c34adf0c1c443283957340ecdc0051db68a
Sagar Dhadke <peaceunique2003@gmail.com>
2025-07-24 12:11:53 +0530

    Initial Commit

---
+++ bin/dart_base.dart
@@ -0,0 +1,5 @@
+import 'package:dart_base/dart_base.dart' as dart_base;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_base.calculate()}!');
+}
---
+++ test/dart_base_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_base/dart_base.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
