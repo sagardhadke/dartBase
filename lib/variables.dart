fd3fc0cace212be323120e56e96aa37f0f972cb8
Sagar Dhadke <peaceunique2003@gmail.com>
2025-07-24 14:28:03 +0530

    Variables

---
+++ lib/variables.dart
@@ -0,0 +1,18 @@
+void main() {
+  String name = "Sagar";
+  int age = 21;
+  bool isMarried = false;
+  List<int> numbers = [12, 52, 123, 568, 454, 65, 78, 5, 46, 5];
+  Map<String, dynamic> mStudents = {
+    "name": "Test 1",
+    "marks": 195,
+    "role": "Flutter Developer",
+    "isActive": true,
+  };
+
+  print("String => $name");
+  print("Int => $age");
+  print("Boolean => $isMarried");
+  print("List => $numbers");
+  print("map => $mStudents");
+}
