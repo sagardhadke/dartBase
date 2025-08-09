611e594331b28a8a6f56d7bf64b75f925a92a303
Sagar Dhadke <peaceunique2003@gmail.com>
2025-08-09 20:03:14 +0530

    Added 4 and 5

---
+++ lib/Practice/List Practice/4.dart
@@ -0,0 +1,11 @@
+void main() {
+  // 4. Remove "banana" from the List and print the updated List.
+
+  List<String> fruits = ["Chiku", "Banana", "Mango", "Kiwi"];
+
+  print("Fruits of list is $fruits");
+
+  fruits.remove('Banana');
+
+  print("New Fruits List $fruits");
+}
---
+++ lib/Practice/List Practice/5.dart
@@ -0,0 +1,9 @@
+void main() {
+  // 5. Check whether a List is empty or not and print the result.
+
+  List<String> names = ["Android", "Ios"];
+
+  print(names.isEmpty);
+
+  print("Check names is empty:- ${names.isEmpty}");
+}
