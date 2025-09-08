d14fa448cf30f42efd0522db79fa71fc51ac25a9
Sagar Dhadke <peaceunique2003@gmail.com>
2025-09-08 17:23:43 +0530

    New Chapter Star

---
+++ lib/Day 1 /dataTypes.dart
@@ -0,0 +1,23 @@
+void main(){
+
+  // * DataTypes
+  
+  //Number -> int,double
+  // String -> string -> sequence of characters 'a' 'b' 'c'
+  // booleans -> true, false -> bool
+  // list -> [] List <> -> generic type
+  // Map -> {} -> key, value pair
+
+  int num = 123;
+  String name = "Sagar";
+  bool online = true;
+  double price = 23.32;
+  List tech = ["Flutter", "Android", "Ios", 'Windows', "MacOs"];
+
+  print("Int $num");
+  print("String $name");
+  print("bool $online");
+  print("Double $price");
+  print("List $tech");
+
+}
