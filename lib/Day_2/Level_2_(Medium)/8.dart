08b8fc6db68bd2698b6cc06fb43b74e8aa7bd82b
Sagar Dhadke <peaceunique2003@gmail.com>
2025-09-10 23:49:33 +0530

    Added 8 Medium Q

---
+++ lib/Day_2/Level_2_(Medium)/8.dart
@@ -0,0 +1,8 @@
+// * Create a list of numbers [10, 20, 30, 40, 50] → Replace index 1 with 99. Print the updated list.
+
+void main() {
+  List<int> numbers = [10, 20, 30, 40, 50];
+  print("Numbers List $numbers");
+  numbers[0] = 99;
+  print("Updated List $numbers");
+}
